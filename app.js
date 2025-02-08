9b7c2fdaca2974b052b85fe65d1fad240665b69b
AmozVoid <159273668+AmozVoid@users.noreply.github.com>
2025-02-08 08:02:14 +0000

    changing back to pages for the main pages from teplates

--- app.js
+++ app.js
@@ -3,28 +3,7 @@ import { Header, Footer} from "./index.js";
 function initApp(){
     const header = new Header();
     header.render();
-
-
     Footer();
 }
 
 initApp();
-
-async function fetchAndRenderTemplate(templatePath) {
-    try {
-      const response = await fetch(templatePath); // Fetch the template file
-      if (response.ok) { // Check if the fetch was successful
-        const templateContent = await response.text(); // Read the content as text
-        document.getElementById('content').innerHTML = templateContent; // Inject it into the page
-      } else {
-        console.error(`Failed to load template: ${response.statusText}`);
-      }
-    } catch (error) {
-      console.error(`Error fetching template: ${error}`);
-    }
-  }
-
-  document.addEventListener('DOMContentLoaded', () => {
-    fetchAndRenderTemplate('./templates/main.html');
-  });
-  
