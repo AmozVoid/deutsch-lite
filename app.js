6a072780d1328726b9f0286f931085f1a6d6475a
AmozVoid <159273668+AmozVoid@users.noreply.github.com>
2025-02-10 08:01:24 +0000

    added the component to app.js and dlt components files

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-import { Header, Footer} from "./index.js";
+// header
+function Header(){
+    const createHeader = document.createElement('header');
+    createHeader.innerHTML = `
+        <h1>Deutsch Lite</h1>
+        <img src="/icons/hamburger.png" alt="hamburger menu"></img>`;
+    document.body.prepend(createHeader);
+    
+};
+
+// footer
+
+function Footer(){
+    const createFooter = document.createElement('footer');
+    createFooter.innerHTML = `<p>all is well...</p>`;
+    document.body.appendChild(createFooter);
+};
 
 function initApp(){
-    const header = new Header();
-    header.render();
+    Header();
     Footer();
 }
 
